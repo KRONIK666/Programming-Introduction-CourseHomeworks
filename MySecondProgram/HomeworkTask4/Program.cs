ee5e2a1f7bf153698551ca3d5a94805583daa7ba
KRONIK666 <kronik@abv.bg>
2015-06-19 00:38:55 +0300

    MySecondProgram

---
+++ MySecondProgram/HomeworkTask4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int z = x % y;
+            Console.WriteLine("Result: " + z);
+        }
+    }
+}
---
+++ MySecondProgram/HomeworkTask5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down a word: ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Write down another word: ");
+            string b = Console.ReadLine();
+
+            string text = a + "_" + b;
+            Console.WriteLine("String: " + text);
+        }
+    }
+}
