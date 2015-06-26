c834676dcba78ebabbb646400b5ca759ea5ac193
KRONIK666 <kronik@abv.bg>
2015-06-27 01:03:51 +0300

    Homeworks

---
+++ Homeworks/HomeworkArrays/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i * 10;
+                Console.WriteLine("Index " + i + " is: " + myArray[i]);
+            } Console.WriteLine();
+        }
+    }
+}
