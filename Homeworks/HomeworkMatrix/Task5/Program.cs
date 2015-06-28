58b2d7f3665410022b3a3d41de06da21272d7c2b
KRONIK666 <kronik@abv.bg>
2015-06-29 01:20:54 +0300

    Homeworks

---
+++ Homeworks/HomeworkMatrix/Task5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of Matrix: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int[,] Matrix = new int[n,n];
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write("Enter element: ");
+                    Matrix[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Your Matrix is:");
+            Console.WriteLine();
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write(Matrix[j,i] + "\t");
+                } Console.WriteLine();
+            } Console.WriteLine();
+        }
+    }
+}
