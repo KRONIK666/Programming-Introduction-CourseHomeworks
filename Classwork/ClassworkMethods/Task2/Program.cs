d85fd0d0477a0b9c42755c44757e56ae0ffe0f3a
KRONIK666 <kronik@abv.bg>
2015-07-03 01:05:07 +0300

    Classwork

---
+++ Classwork/ClassworkMethods/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter Y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int sum = SumIt(x, y);
+            Console.WriteLine();
+            Console.WriteLine("x + y = " + sum);
+            Console.WriteLine();
+        }
+
+        private static int SumIt(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+    }
+}
---
+++ Classwork/ClassworkMethods/Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length: ");
+            int n = int.Parse(Console.ReadLine());
+            string[] massive = new string[n];
+
+            for (int i = 0; i < massive.Length; i++)
+            {
+                Console.Write("Enter element: ");
+                massive[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Your array is:");
+            Console.WriteLine();
+
+            Array(massive);
+
+            Console.WriteLine();
+        }
+
+        private static void Array(string[] massive)
+        {
+            for (int i = massive.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(massive[i]);
+            }
+        }
+    }
+}
