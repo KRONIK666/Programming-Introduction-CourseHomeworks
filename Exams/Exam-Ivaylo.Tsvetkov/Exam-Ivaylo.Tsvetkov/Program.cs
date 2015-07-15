26588d492c1a4b38c83a72607303bae874bb1309
KRONIK666 <kronik@abv.bg>
2015-07-15 11:59:14 +0300

    Exams

--- Exams/Exam-Ivaylo.Tsvetkov/Exam-Ivaylo.Tsvetkov/Program.cs
+++ Exams/Exam-Ivaylo.Tsvetkov/Exam-Ivaylo.Tsvetkov/Program.cs
@@ -10,47 +10,70 @@ namespace Exam_Ivaylo.Tsvetkov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Въведете число в интервала от 2 до 9:");
+            Console.Write("Enter length of myArray: ");
             int n = int.Parse(Console.ReadLine());
+            int m = n * n;
+            int[] myArray = new int[m];
 
-            int[,] MyArray = new int[n, n];
-            Console.WriteLine("Броят на необходимите елементи за попълване на масива е:" + MyArray.Length);
-
-            int[] MyArray2 = new int[MyArray.Length];
-
-            Console.WriteLine("Въведете числа, за да попълните масива:", MyArray.Length);
-
-            for (int i = 0; i < MyArray.Length; i++)
+            if (n >= 2 && n <= 9)
             {
-                MyArray2[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.Write("Enter an element: ");
+                    myArray[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Start again! Please, enter a valid value!");
+                Console.WriteLine();
+                return;
+            }
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("myArray is:");
+            Console.WriteLine();
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
             }
 
             int sum = 0;
-            foreach (int item in MyArray2)
-            {
-                sum += item;
-            }
-            Console.WriteLine("Сумата от всички въведени елементи в масива е:" + sum);
-
-            int c = MyArray2[0];
-
-
-            for (int i = 0; i < MyArray.GetLength(0); i++)
-            {
-                for (int j = 0; j < MyArray.GetLength(1); j++)
+            
+            foreach (var item in myArray)
                 {
-                    MyArray[i, j] = c++;
+                    sum = item + sum;
+                }
+
+            Console.WriteLine();
+            Console.WriteLine("Sum of all elements in myArray: " + sum);
+
+            int[,] Matrix = new int[n,n];
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Your Matrix is:");
+            Console.WriteLine();
+
+            int l = 0;
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Matrix[j, i] = myArray[l++];
                 }
             }
-            Console.WriteLine("Матрицата от въведените числа е:");
-            for (int i = 0; i < MyArray.GetLength(0); i++)
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < MyArray.GetLength(1); j++)
+                for (int j = 0; j < Matrix.GetLength(1); j++)
                 {
-                    Console.Write("      " + MyArray[j, i]);
-                }
-                Console.WriteLine();
-            }
+                    Console.Write(Matrix[i, j] + "\t");
+                } Console.WriteLine();
+            } Console.WriteLine();
         }
     }
 }
