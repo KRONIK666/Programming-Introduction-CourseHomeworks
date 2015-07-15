706f78e743534672a6fe8401848d8a637dfd4441
KRONIK666 <kronik@abv.bg>
2015-07-15 12:04:14 +0300

    Exams

---
+++ Exams/ExamPreparation/ExamPreparation3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparation3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Matrix = new int[,] {
+            { 8, 17, 28, 9, 10, 33, 16, 13, 24, 30 },
+            { 88, 2, 79, 95, 4, 14, 66, 3, 44, 6 }
+            };
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            int ColMinIndex = 0;
+            int RowMinIndex = 0;
+            int ColMaxIndex = 0;
+            int RowMaxIndex = 0;
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    if (Matrix[i, j] < min)
+                    {
+                        min = Matrix[i, j];
+                        ColMinIndex = i;
+                        RowMinIndex = j;
+                    }
+                    if (Matrix[i, j] > max)
+                    {
+                        max = Matrix[i, j];
+                        ColMaxIndex = i;
+                        RowMaxIndex = j;
+                    }
+                }
+            }
+            Console.WriteLine("Min valued element is {0} with index [{1},{2}];", min, ColMinIndex, RowMinIndex);
+            Console.WriteLine("Max valued element is {0} with index [{1},{2}];", max, ColMaxIndex, RowMaxIndex);
+        }
+    }
+}
---
+++ Exams/ExamPreparation/ExamPreparation4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPreparation4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1st number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+            Console.WriteLine();
+
+            if (a > b)
+            {
+                c = a;
+                a = b;
+                b = c;
+                Console.WriteLine("New value for a: " + a);
+                Console.WriteLine("New value for b: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Can't switch numbers!");
+            }
+            Console.WriteLine();
+        }
+    }
+}
